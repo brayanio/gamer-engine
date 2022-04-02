90d54c4f93a7e358e4df0bd6bbdb8f4f103a1509
Todd Lewis aka Brayan <brayanbyrdsong@icloud.com>
2022-04-01 19:08:55 -0600

    fire moves better... sorta

--- behavior/projectile.js
+++ behavior/projectile.js
@@ -5,17 +5,22 @@ export default gamer.behavior(
   'projectile',
   {
     speed: 12,
-    setSpeed: (sprite, n) => sprite.speed = n,
+    setSpeed: (sprite, n) => {
+      sprite.speed = n
+    },
     target: null,
-    setTarget: (sprite, t) => sprite.target = t,
+    setTarget: (sprite, t) => {
+      sprite.target = t
+    },
     projectileHitFN: emptyfn,
     onProjectileHit: (sprite, fn) => sprite.projectileHitFN = fn,
 
     projectileMovement: sprite => {
       if(sprite.target){
-        let x = sprite.getCenter().x > sprite.target.getCenter().x ? -sprite.speed : sprite.speed
-        let y = sprite.getCenter().y > sprite.target.getCenter().y ? -sprite.speed : sprite.speed
-        sprite.move(x, y)
+        sprite.move(
+          -((sprite.getBounds().x - sprite.target.getBounds().x) / sprite.speed),
+          -((sprite.getBounds().y - sprite.target.getBounds().y) / sprite.speed)
+        )
         if(sprite.isTouching( sprite.target )){
           sprite.projectileHitFN()
           sprite.destroy()
